5e8287a3047af7e4b1fb5890a9792a9bbf160c6c
vilyLei <vily313@126.com>
2023-05-04 09:19:33 +0800

    fix: add the project configs.

---
+++ src/demo/DemoBase.ts
@@ -0,0 +1,11 @@
+
+export class DemoBase {
+
+    initialize(): void {
+		console.log("DemoBase::initialize() ...");
+    }
+    run(): void {
+    }
+}
+
+export default DemoBase;
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+
+import {DemoBase as Demo} from "./demo/DemoBase";
+
+document.title = "web os file system";
+let ins = new Demo();
+function main(): void {
+    console.log("------ demo --- init ------");
+    ins.initialize();
+    function mainLoop(now: any): void {
+        ins.run();
+        window.requestAnimationFrame(mainLoop);
+    }
+    window.requestAnimationFrame(mainLoop);
+    console.log("------ demo --- running ------");
+}
+main();
